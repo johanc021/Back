0b927a597178fe9733f4224ee1b103b13f48784d
Johan <facjohan@hotmail.com>
2023-06-22 19:29:58 -0500

    Engrega 4

---
+++ Modulo 1/5. Entrega 4/src/routes/views.router.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import fs from 'fs'
+import { socketServer } from '../App.js';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    const data = fs.readFileSync('./products.json', 'utf-8');
+    const products = JSON.parse(data);
+
+    // Emitir los productos al cliente a través de Socket.IO
+    socketServer.emit('updateProducts', products);
+
+    res.render('realTimeProducts', { products });
+});
+
+
+
+export default router;
