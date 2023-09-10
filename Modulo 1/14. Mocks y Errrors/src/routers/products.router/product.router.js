f6fac83ebd2a8c3f52c7a43e34cdc0add982d98d
Johan Miguel Castellanos Carpeta <facjohan@hotmail.com>
2023-09-10 18:46:30 -0500

    Agregando Compression

--- Modulo 1/14. Mocks y Errrors/src/routers/products.router/product.router.js
+++ Modulo 1/14. Mocks y Errrors/src/routers/products.router/product.router.js
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import productController from '../../controllers/product.controller.js';
 import isAdmin from '../../config/middlewareAuth/authRole/isAdmin.js';
 import { authenticate } from '../../config/middlewareAuth/authAuthenticate/authenticate.js';
+import compression from 'express-compression'
 
 class ProductRouter {
     constructor() {
         this.router = Router();
-        this.router.get('/', productController.getAllProducts);
+        this.router.get('/', compression(), productController.getAllProducts);
         this.router.get('/:productId', productController.getProductById);
         this.router.post('/', authenticate, isAdmin, productController.createProduct);
         this.router.put('/:productId', authenticate, isAdmin, productController.updateProduct);
